6c26d10b030be5e404cde6219437051d2506e391
rizwan-ilyas <rizwanilyas.me@gmail.com>
2022-07-21 21:14:24 +0500

    commit outputpics

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,4 @@
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -44,7 +42,8 @@ public class Client {
 
     // function defination of inintGUI function
     void initGUI() {
-        frame = new JFrame();     // initialzing the frame
+        frame = new JFrame("Let's Talk");     // initialzing the frame
+        frame.setIconImage(Toolkit.getDefaultToolkit().getImage("icon.png"));
         frame.setLayout(new FlowLayout());  // setting the layout of the frame
 
         txtArea = new JTextArea(20, 35);
